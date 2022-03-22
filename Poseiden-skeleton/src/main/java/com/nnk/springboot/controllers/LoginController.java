77b89a2db74609ac44c87595fc97f6a0326ccf25
Sabouni Youssef <youssefsabouni@icloud.com>
2022-03-22 09:21:24 +0100

    feature(LoginController): Add recorders for each endpoint and add a new user to the model

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,7 +1,11 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +15,27 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("app")
 public class LoginController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("login")
     public ModelAndView login() {
+
+        LOGGER.info("LoginController.class | Get Request: \"/login\"");
+
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
+        mav.addObject("user", new User());
         return mav;
     }
 
     @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
+
+        LOGGER.info("LoginController.class | Get Request: \"/secure/article-details\"");
+
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userRepository.findAll());
         mav.setViewName("user/list");
@@ -31,8 +44,11 @@ public class LoginController {
 
     @GetMapping("error")
     public ModelAndView error() {
-        ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
+
+        LOGGER.info("LoginController.class | Get Request: \"/error\"");
+
+        ModelAndView mav          = new ModelAndView();
+        String       errorMessage = "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
         mav.setViewName("403");
         return mav;
