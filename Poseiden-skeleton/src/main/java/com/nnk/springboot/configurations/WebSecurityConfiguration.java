f021136fe4ba545c83526f38c0ef261c41a3466f
Sabouni Youssef <youssefsabouni@icloud.com>
2022-03-22 09:16:08 +0100

    feature(WebSecurityConfiguration): Adding the hash and authentification methods, configuration of requests for anonymous and authenticated persons

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,103 @@
+package com.nnk.springboot.configurations;
+
+import com.nnk.springboot.services.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    /**
+     * Create a new instance of {@link WebSecurityConfiguration}. This will be done automatically by SpringBoot with
+     * dependency injection.
+     *
+     * @param userService
+     *         instance of {@link UserService}.
+     */
+    public WebSecurityConfiguration(UserService userService) {
+
+        this.userService = userService;
+    }
+
+    /**
+     * Any endpoint that requires defense against common vulnerabilities can be specified here, including public ones.
+     *
+     * @param http
+     *         the {@link HttpSecurity} to modify
+     *
+     * @throws Exception
+     *         if an error occurs.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+            .antMatchers("/static/**", "/h2-console/**")
+            .permitAll()
+            .antMatchers("/login*")
+            .anonymous()
+            .antMatchers("/admin*")
+            .hasRole("ADMIN")
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/bidList/list")
+            .failureUrl("/login?error")
+            .and()
+            .oauth2Login()
+            .loginPage("/app-login")
+            .defaultSuccessUrl("/bidList/list")
+            .and()
+            .logout()
+            .logoutUrl("/app-logout")
+            .logoutSuccessUrl("/login?logout");
+    }
+
+    /**
+     * Allows you to specify the {@link AuthenticationManager}.
+     *
+     * @param authManagerBuilder
+     *         allows for easily building in memory authentication
+     *
+     * @throws Exception
+     *         if an error occurs.
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+
+        authManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    /**
+     * Expose le {@link AuthenticationManager} en tant que Bean.
+     *
+     * @return the {@link AuthenticationManager}.
+     *
+     * @throws Exception
+     *         if an error occurs.
+     */
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+}
