1a06be43e4df368792e16802348c720ec9af79d3
Sabouni Youssef <youssefsabouni@icloud.com>
2022-03-23 21:07:41 +0100

    feature(Controllers): Add logger for each request

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -5,7 +5,6 @@ import com.nnk.springboot.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
