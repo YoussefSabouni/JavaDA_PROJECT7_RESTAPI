9c5928de22609f3799b503742cb40c0f83b42301
Sabouni Youssef <youssefsabouni@icloud.com>
2022-03-24 12:03:42 +0100

    feature(ApplicationTest): add a test class to check that the test structure works

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.nnk.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
