c73d3f4db8b471bd457205af5d396446add6db3f
Sabouni Youssef <youssefsabouni@icloud.com>
2022-04-18 08:01:27 +0200

    feature(UserService): Add user management methods

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/UserService.java
@@ -1,11 +1,15 @@
 package com.nnk.springboot.services;
 
 
+import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -31,5 +35,31 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         return this.userRepository.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+
+        return this.userRepository.findAll();
+    }
+
+    public User findById(Integer id) {
+
+        return this.userRepository.findById(id)
+                                     .orElseThrow(() -> new NoSuchElementException("Invalid user Id:" + id));
+    }
+
+    public User save(User user) {
+
+        return this.userRepository.save(user);
+    }
+
+    public void deleteById(Integer id) {
+
+        if (!this.userRepository.findById(id).isPresent()) {
+
+            throw new NoSuchElementException("Invalid user Id:" + id);
+        }
+
+        this.userRepository.deleteById(id);
     }
 }
